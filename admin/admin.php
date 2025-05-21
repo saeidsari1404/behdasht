32c8e1c896aec9650255b35ba8b9938bdf3554c2
saeidsari1404 <behdasht.ac.ir@gmail.com>
2025-05-20 22:36:24 -0700

    Delete admin directory

--- admin/admin.php
+++
@@ -1,7 +0,0 @@
-<!-- admin.php -->
-<?php include 'includes/header.php'; ?>
-
-<h2>پنل مدیریت</h2>
-<p>به بخش مدیریت خوش آمدید. در اینجا می‌توانید کاربران و داده‌های سایت را مدیریت کنید.</p>
-
-<?php include 'includes/footer.php'; ?>
