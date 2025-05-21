83e80b6042ee2d6fe855d635d08e4d1a194cee1f
saeidsari1404 <behdasht.ac.ir@gmail.com>
2025-05-20 22:36:00 -0700

    Delete pages directory

--- pages/dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php include 'includes/header.php'; ?>
-
-<h2>داشبورد کاربران</h2>
-<p>خوش آمدید! اینجا پنل کاربری شماست.</p>
-
-<?php include 'includes/footer.php'; ?>
--- pages/indicators.php
+++
@@ -1,6 +0,0 @@
-<?php include 'includes/header.php'; ?>
-
-<h2>شاخص‌گذاری</h2>
-<p>در این صفحه می‌توانید شاخص‌های سلامت را مدیریت کنید.</p>
-
-<?php include 'includes/footer.php'; ?>
